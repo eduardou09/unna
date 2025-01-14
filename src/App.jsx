ebf3258cf9ed651f03ce896857190e25542a23b8
Eduardo Ulysséa <eduardoulyssea@MacBook-Air-de-Eduardo.local>
2025-01-14 03:08:14 -0300

    finish visual

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,8 @@ import styles from "./App.module.css";
 import LocationValue1 from "./components/hero/locationValue/location";
 import LocationValue2 from "./components/hero/locationValue2/location";
 import Infrastructure from "./components/hero/infrastructure/infrastructure";
+import TypeStudio from "./components/hero/typesStudio/typesStudio";
+import How from "./components/hero/how/how";
 
 
 export default function App() {
@@ -16,6 +18,8 @@ export default function App() {
 			<LocationValue1/>
 			<LocationValue2/>
 			<Infrastructure/>
+			<TypeStudio/>
+			<How/>
 		</div>
 	);
 }
---
+++ src/components/hero/typesStudio/typesStudio.jsx
@@ -0,0 +1,42 @@
+import { useState } from "react";
+import styles from "./typesStudio.module.css";
+
+export default function TypeStudio() {
+  const [count, setCount] = useState("1");
+  return (
+    <article className={styles.info}>
+      <div className={styles.buttonsLine}>
+        <div className={styles.buttons} onClick={() => setCount("1")}>
+          Studio Garden
+        </div>
+        <div className={styles.buttons} onClick={() => setCount("2")}>
+          Studio
+        </div>
+        <div className={styles.buttons} onClick={() => setCount("3")}>
+          Cobertura
+        </div>
+      </div>
+      {count === "1" && 
+      <div className={styles.aps}>
+        <img src="images/interior-studio.jpg" alt="" srcset="" />
+        <h1>Studio Garden</h1>
+        <p>Os studios do Unna foram projetados para oferecer o máximo em funcionalidade e conforto em um espaço compacto e moderno. Sua localização estratégica é ideal para estudantes e profissionais, garantindo praticidade e alto potencial de valorização.</p>
+        <button className={styles.investing}>Invista já</button>
+        </div> }
+        {count === "2" && 
+      <div className={styles.aps}>
+        <img src="images/interior-studio.jpg" alt="" srcset="" />
+        <h1>Studio</h1>
+        <p>Os studios do Unna foram projetados para oferecer o máximo em funcionalidade e conforto em um espaço compacto e moderno. Sua localização estratégica é ideal para estudantes e profissionais, garantindo praticidade e alto potencial de valorização.</p>
+        <button className={styles.investing}>Invista já</button>
+        </div> }
+        {count === "3" && 
+      <div className={styles.aps}>
+        <img src="images/interior-studio.jpg" alt="" srcset="" />
+        <h1>Cobertura</h1>
+        <p>Os studios do Unna foram projetados para oferecer o máximo em funcionalidade e conforto em um espaço compacto e moderno. Sua localização estratégica é ideal para estudantes e profissionais, garantindo praticidade e alto potencial de valorização.</p>
+        <button className={styles.investing}>Invista já</button>
+        </div> }
+    </article>
+  );
+}
