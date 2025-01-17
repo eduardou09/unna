486d05b228bfc1b47758686a322c4b3d57d42059
pevss <pedrov3245@gmail.com>
2025-01-17 00:02:30 -0300

    alteracoes-peve

--- src/components/hero/how/how.jsx
+++ src/components/hero/how/how.jsx
@@ -1,108 +1,128 @@
 import { useState } from "react";
 import styles from "./how.module.css";
 import { alertError } from "../../Alert/alert";
-import { isMobile } from "react-device-detect";
+
+const steps = [
+	{
+		title: "1. Estudo de Mercado e Aquisição do Terreno",
+		text: "A administradora localiza o melhor empreendimento para voce e negocia a compra do terreno.",
+	},
+	{
+		title: "2. Construção da SPE",
+		text: "Investidores formam uma sociedade (SPE), afim de construir e incorporar o empreendimento imobiliário.",
+	},
+	{
+		title: "3. Captação de recursos",
+		text: "Investidores realizam aportes mensais desde o pagamento do terreno até a conclusão da obra, mediante ao custo real dela.",
+	},
+	{
+		title: "4. Execução e Entrega da Obra",
+		text: "Matrículas são individualizadas e ocorre a entrega das unidade.",
+	},
+	{
+		title: "5. Encerramento da SPE",
+		text: "A sociedade (SPE) é extinta.",
+	},
+];
 
 export default function How() {
-  const [name, setName] = useState("")
-  const [email, setEmail] = useState("")
-  const [phone, setPhone] = useState("")
+	const [name, setName] = useState("");
+	const [email, setEmail] = useState("");
+	const [phone, setPhone] = useState("");
 
-  function sendEmal (e){
-    e.preventDefault()
+	function sendEmal(e) {
+		e.preventDefault();
 
-    if(name === "" || email === "" || phone === "" ){
-      alertError("Preencha todos os dados")
-      return
-    } else {
-      // Se todos os campos estiverem preenchidos, cria o link do WhatsApp
-      const whatsappLink = `https://wa.me/5548996744426?text=Olá.%20Estou%20interessado%20em%20receber%20mais%20informações%20do%20UNNA%20STUDIOS.%20Meu%20nome%20é%20${encodeURIComponent(name)}%20e%20meu%20email%20é%20${encodeURIComponent(email)}%20e%20meu%20telefone%20é%20${encodeURIComponent(phone)}.`;
+		if (name === "" || email === "" || phone === "") {
+			alertError("Preencha todos os dados");
+			return;
+		} else {
+			// Se todos os campos estiverem preenchidos, cria o link do WhatsApp
+			const whatsappLink = `https://wa.me/5548996744426?text=Olá.%20Estou%20interessado%20em%20receber%20mais%20informações%20do%20UNNA%20STUDIOS.%20Meu%20nome%20é%20${encodeURIComponent(
+				name
+			)}%20e%20meu%20email%20é%20${encodeURIComponent(
+				email
+			)}%20e%20meu%20telefone%20é%20${encodeURIComponent(phone)}.`;
 
-    // Redireciona para o link do WhatsApp
-    window.location.href = whatsappLink;
-    }
-  }
-  return (
-    <article className={styles.info}>
-      <div className={styles.whoFunc}>
-        <h2>COMO FUNCIONA UMA SPE?</h2>
-        <div>
-          <p>
-            A Sociedade de Propósito Específico (SPE) é uma estrutura jurídica
-            amplamente utilizada no setor imobiliário, especialmente em projetos
-            de incorporação. Trata-se de uma empresa constituída com o objetivo
-            exclusivo de executar um determinado empreendimento, e sua
-            existência está diretamente ligada à conclusão do projeto. Após a
-            conclusão, a SPE é encerrada.
-          </p>
-        </div>
-        <div className={styles.numbers}>
-        <div className={styles.skus}>
-          <h1>1. Estudo de Mercado e Aquisição do Terreno </h1>
-          <p>
-            A administradora localiza o melhor empreendimento para voce e
-            negocia a compra do terreno.
-          </p>
-        </div>
-        <div className={styles.skus}>
-          <h1>2. Construção da SPE </h1>
-          <p>
-            Investidores formam uma sociedade (SPE), afim de construir e
-            incorporar o empreendimento imobiliário.
-          </p>
-        </div>
-        <div className={styles.skus}>
-          <h1>3. Captação de recursos</h1>
-          <p>
-            Investidores realizam aportes mensais desde o pagamento do terreno
-            até a conclusão da obra, mediante ao custo real dela.
-          </p>
-        </div>
-        <div className={styles.skus}>
-          <h1>4. Execução e Entrega da Obra</h1>
-          <p>Matrículas são individualizadas e ocorre a entrega das unidade.</p>
-        </div>
-        <div className={styles.skusD}>
-          <h1>5. Encerramento da SPE</h1>
-          <p>A sociedade (SPE) é extinta.</p>
-        </div>
-      </div>
-      </div>
+			// Redireciona para o link do WhatsApp
+			window.location.href = whatsappLink;
+		}
+	}
+	return (
+		<article className={styles.info}>
+			<div className={styles.whoFunc}>
+				<h2>COMO FUNCIONA UMA SPE?</h2>
 
-      
-      <div className={styles.container}>
-       
-        <img src="images/interior-studioCama.png" alt="interior-studioCama" />
-        <form className={styles.formMain} onSubmit={sendEmal}>
-          <div className={styles.input}>
-            <label htmlFor="">Nome</label>
-            <input type="text" 
-             onChange={(e)=> setName(e.target.value)}
-             value={name}
-            />
-          </div>
-          <div className={styles.form}>
-            <div className={styles.input}>
-              <label htmlFor="">Email</label>
-              <input type="text"
-              onChange={(e)=> setEmail(e.target.value)}
-              value={email}
-              />
-            </div>
-            <div className={styles.input}>
-              <label htmlFor="">Celular</label>
-              <input type="text"
-               onChange={(e)=> setPhone(e.target.value)}
-               value={phone}
-              />
-            </div>
-          </div>
-          <button className={styles.investing} type="submit" value="Enviar">
-            Torne-se um investidor
-          </button>
-        </form >
-      </div>
-  
-    </article>
-  );
+				<div>
+					<p>
+						A Sociedade de Propósito Específico (SPE) é uma
+						estrutura jurídica amplamente utilizada no setor
+						imobiliário, especialmente em projetos de incorporação.
+						Trata-se de uma empresa constituída com o objetivo
+						exclusivo de executar um determinado empreendimento, e
+						sua existência está diretamente ligada à conclusão do
+						projeto. Após a conclusão, a SPE é encerrada.
+					</p>
+				</div>
+
+				<div className={styles.numbers}>
+					{steps.map((step, i) => {
+						return (
+							<div
+								className={
+									i === steps.length - 1
+										? styles.skusD
+										: styles.skus
+								}
+							>
+								<h1>{step.title}</h1>
+								<p>{step.text}</p>
+							</div>
+						);
+					})}
+				</div>
+			</div>
+
+			<div className={styles.container}>
+				<img src="images/interior-studio.jpg" alt="bg" />
+				<form className={styles.formMain} onSubmit={sendEmal}>
+					<div className={styles.input}>
+						<label htmlFor="">Nome</label>
+						<input
+							type="text"
+							onChange={(e) => setName(e.target.value)}
+							value={name}
+						/>
+					</div>
+
+					<div className={styles.form}>
+						<div className={styles.input}>
+							<label htmlFor="">Email</label>
+							<input
+								type="text"
+								onChange={(e) => setEmail(e.target.value)}
+								value={email}
+							/>
+						</div>
+						<div className={styles.input}>
+							<label htmlFor="">Celular</label>
+							<input
+								type="text"
+								onChange={(e) => setPhone(e.target.value)}
+								value={phone}
+							/>
+						</div>
+					</div>
+
+					<button
+						className={styles.investing}
+						type="submit"
+						value="Enviar"
+					>
+						Torne-se um investidor
+					</button>
+				</form>
+			</div>
+		</article>
+	);
 }
--- src/components/hero/typesStudio/typesStudio.jsx
+++ src/components/hero/typesStudio/typesStudio.jsx
@@ -1,71 +1,58 @@
 import { useState } from "react";
 import styles from "./typesStudio.module.css";
 
-const types = {
-  1 : {
-    img : "images/interior-studio.jpg",
-    title: "Studio Garden",
-    text: "Os studios do Unna foram projetados para oferecer o máximo em funcionalidade e conforto em um espaço compacto e moderno. Sua localização estratégica é ideal para estudantes e profissionais, garantindo praticidade e alto potencial de valorização."
-  },
-  2 : {
-    img : "images/studioC.png",
-    title: "Studio",
-    text: "Os studios do Unna foram projetados para oferecer o máximo em funcionalidade e conforto em um espaço compacto e moderno. Sua localização estratégica é ideal para estudantes e profissionais, garantindo praticidade e alto potencial de valorização."
-  },
-  3 : {
-    img : "images/coberturaC.png",
-    title: "Cobertura",
-    text: "Os studios do Unna foram projetados para oferecer o máximo em funcionalidade e conforto em um espaço compacto e moderno. Sua localização estratégica é ideal para estudantes e profissionais, garantindo praticidade e alto potencial de valorização."
-  }
-}
+const types = [
+	{
+		img: "images/interior-studio.jpg",
+		title: "Studio Garden",
+		text: "Os studios do Unna foram projetados para oferecer o máximo em funcionalidade e conforto em um espaço compacto e moderno. Sua localização estratégica é ideal para estudantes e profissionais, garantindo praticidade e alto potencial de valorização.",
+	},
+	{
+		img: "images/studioC.png",
+		title: "Studio",
+		text: "Os studios do Unna foram projetados para oferecer o máximo em funcionalidade e conforto em um espaço compacto e moderno. Sua localização estratégica é ideal para estudantes e profissionais, garantindo praticidade e alto potencial de valorização.",
+	},
+	{
+		img: "images/coberturaC.png",
+		title: "Cobertura",
+		text: "Os studios do Unna foram projetados para oferecer o máximo em funcionalidade e conforto em um espaço compacto e moderno. Sua localização estratégica é ideal para estudantes e profissionais, garantindo praticidade e alto potencial de valorização.",
+	},
+];
 
 export default function TypeStudio() {
-  const [count, setCount] = useState("1");
-  return (
-    <article className={styles.info}>
-      <div className={styles.buttonsLine}>
-        <div
-          className={`${styles.buttons} ${count === "1" ? styles.active : ""}`}
-          onClick={() => setCount("1")}
-        >
-          Studio Garden
-        </div>
-        <div
-          className={`${styles.buttons} ${count === "2" ? styles.active : ""}`}
-          onClick={() => setCount("2")}
-        >
-          Studio
-        </div>
-        <div
-          className={`${styles.buttons} ${count === "3" ? styles.active : ""}`}
-          onClick={() => setCount("3")}
-        >
-          Cobertura
-        </div>
-      </div>
-      <div className={styles.aps}>
+	const [count, setCount] = useState(0);
 
-          <img
-            src={types[count].img}
-            alt="interior-studio"
-            srcset=""
-          />
-          <div className={styles.information}>
-            <h1>{types[count].title}</h1>
-            <p>
-            {types[count].text}
-            </p>
-            <a
-              href="https://wa.me/5548996744426"
-              target="_blank"
-              rel="noopener noreferrer"
-              className={styles.investing}
-            >
-              {" "}
-              Invista já
-            </a>
-          </div>
-        </div>
-    </article>
-  );
+	return (
+		<article className={styles.info}>
+			<div className={styles.buttonsLine}>
+				{types.map((type, i) => (
+					<div
+						className={`${styles.buttons} ${
+							count === i ? styles.active : ""
+						}`}
+						onClick={() => setCount(i)}
+					>
+						{type.title}
+					</div>
+				))}
+			</div>
+
+			<img src={types[count].img} alt="interior-studio" srcset="" />
+
+			<div className={styles.information}>
+				<div className={styles.informationTexts}>
+					<h1>{types[count].title}</h1>
+					<p>{types[count].text}</p>
+				</div>
+				<a
+					className={styles.investing}
+					href="https://wa.me/5548996744426"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					Invista já
+				</a>
+			</div>
+		</article>
+	);
 }
